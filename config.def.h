b5b4cad7b9f016ff84302f7b5017eab203694194
emanueljg <emanueljohnsongodin@gmail.com>
2025-11-17 21:09:58 +0100

    keyboard

--- config.def.h
+++ config.def.h
@@ -57,7 +57,9 @@ static const struct xkb_rule_names xkb_rules = {
 	/* example:
 	.options = "ctrl:nocaps",
 	*/
-	.options = NULL,
+	.layout  = "us",
+	.variant = "altgr-intl",
+	.options = "caps:swapescape",
 };
 
 static const int repeat_rate = 35;
